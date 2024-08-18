5e6663d6403e04d77090e013b00539e8a8b90124
Khaled alshammari <eng.Khaled10@outlook.com>
2024-08-18 20:57:38 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "./assest/css/day1.css";
+import cookies from "./assest/media/cookieShop.jpg";
+import menu from "./assest/data";
 function App() {
+  const prudect = menu.map((item) => (
+    <div className="menu-section">
+      <img src={item.image} alt={item.name} />
+      <h2>{item.name}</h2>
+      <p>{item.price}</p>
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Khaled's cookies</h1>
+      <p>where the best cookies in the world</p>
+      <img src={cookies} alt="Khaled's cookies" className="img-style" />
+
+      <div className="menu-container">{prudect}</div>
     </div>
   );
 }
---
+++ src/assest/data.js
@@ -0,0 +1,16 @@
+const menu = [
+  {
+    name: "Orange and milk",
+    image:
+      "https://www.benscookies.com.kw/wp-content/uploads/sites/6/2016/04/orange-and-milk-260x260.png",
+    price: `2.500KD`,
+  },
+  {
+    name: "Dark chocolate",
+    image:
+      "https://www.benscookies.com.kw/wp-content/uploads/sites/6/2016/04/orange-and-milk-260x260.png",
+    price: ` 1.900 KD`,
+  },
+];
+
+export default menu;
